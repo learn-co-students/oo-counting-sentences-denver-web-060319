930ea55537f75da937b0298c068cecdbe65c7e92
github username <github email address>
2019-06-07 09:02:01 -0600

    Use reduce

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,11 +16,12 @@ class String
 
   def count_sentences
     count = 0
-    self.split(" ").each do |str|
+    self.split(" ").reduce(0) do |sum, str|
       if str.sentence? || str.question? || str.exclamation?
-        count += 1
+        sum + 1
+      else
+        sum
       end
     end
-    count
   end
 end
